0eaa4d2f528cf0cd6da50a5b2d2cee10e6960249
dushimimana diane <dushimedian12@gmail.com>
2022-04-12 21:08:43 +0200

    javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+let name = 'Diane';
+console.log(name);
+let FirstName='diane';
+let LastName='Dushimimana';
+console.log(FirstName);
+console.log(LastName);
+let interstRate= 0.3;
+interstRate= 1;
+console.log(interstRate);
+let isApproved= false;
+
+let person ={
+    name : 'Alice',
+    age : 30
+};
